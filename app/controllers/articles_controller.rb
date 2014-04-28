504e35bb97adb84261dc52280f74c50cfc39b344
k1ife <fork1ife@gmail.com>
2014-04-28 11:32:23 +0400

    add submit_or_cancel to users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,12 +24,13 @@ class ArticlesController < ApplicationController
   # GET /articles/1/edit
   def edit
     @title = 'Редактирование статьи - ' + @article.title
+    @article = current_user.articles.find(params[:id])
   end
 
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.new(article_params)
 
     respond_to do |format|
       if @article.save
@@ -45,6 +46,7 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
+    @article = current_user.articles.find(params[:id])
     respond_to do |format|
       if @article.update(article_params)
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
@@ -59,6 +61,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    @article = current_user.articles.find(params[:id])
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def submit_or_cancel(form, name="Cancel")
+    form.submit(:class => 'btn btn-success') + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'btn btn-danger')
+  end
 end
