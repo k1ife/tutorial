78e511e549a22cddc4fe80d2c7bd87812e65a4f1
k1ife <fork1ife@gmail.com>
2014-04-28 11:48:39 +0400

    add user_bar and some style to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def submit_or_cancel(form, name="Cancel")
-    form.submit(:class => 'btn btn-success') + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'btn btn-danger')
+  def submit_or_cancel(form, name=t('cancel'))
+    form.submit( t('save'), :class => 'btn btn-success') + "  " + link_to(name, 'javascript:history.go(-1);', :class => 'btn btn-danger')
   end
 end
